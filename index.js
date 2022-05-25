eb09db8677fedc2e286ed7c87c5802e8498c14ce
bvlucretiu <vasilebirta@gmail.com>
2022-05-25 15:06:56 +0300

    edid index js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-displayCurrentWeather();
+let currentCityLS = localStorage.getItem("city");
+if (currentCityLS === null) {
+  currentCityLS = "București";
+}
+
+const currentCity = document.querySelector("#current-city");
+currentCity.innerHTML = currentCityLS;
+
+displayCurrentWeather(currentCityLS);
