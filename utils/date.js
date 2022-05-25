d961da5da9f015c37b7d4df770549248798ff1b2
bvlucretiu <vasilebirta@gmail.com>
2022-05-25 10:10:42 +0300

    edit file date

--- utils/date.js
+++ utils/date.js
@@ -1,9 +1,11 @@
-function getWeekDay(unixUtc) {
-  let date = new Date(unixUtc * 1000);
-  let weekDay = date.getDay();
-  console.log(weekDay);
+function getDayOfTheWeek(utc) {
+  // Pentru a crea o data, pornind la o valoare unix utc, este nevoie sa o inmultim cu 1000 mai intai.
+  const date = new Date(utc * 1000);
+  // Extragem ziua saptamanii, sub forma de index.
+  const dayIndex = date.getDay();
   let day;
-  switch (weekDay) {
+
+  switch (dayIndex) {
     case 0:
       day = "Duminică";
       break;
@@ -24,21 +26,29 @@ function getWeekDay(unixUtc) {
       break;
     case 6:
       day = "Sâmbătă";
+      break;
+    default:
+      // Aruncam o eroare daca index-ul nu este valid (nu ar trebui sa se ajunga vreodata aici).
+      throw new Error("Indexul trebuie sa fie intre 0 si 6.");
   }
+
+  // Retunam ziua echivalenta indexului.
   return day;
 }
 
-function getHours(unixUtc) {
-  let date = new Date(unixUtc * 1000);
-
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-  const secounds = date.getSeconds();
-  return `${hour}:${minutes}:${secounds}`;
+function getHour(utc) {
+  // Pentru a crea o data, pornind la o valoare unix utc, este nevoie sa o inmultim cu 1000 mai intai.
+  const date = new Date(utc * 1000);
+  // Extragem ora. Daca ora are o valoare mai mica de 10, ii adaugam un 0.
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  // Extragem minutele. Daca minutele au o valoare mai mica de 10, le adaugam un 0.
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  // Returnam ora, sub formatul dorit.
+  return `${hours}:${minutes}`;
 }
-// function timeIs(number) {
-//   if (number < 10) {
-//     number = "0" + number;
-//   }
-//   return number;
-// }
