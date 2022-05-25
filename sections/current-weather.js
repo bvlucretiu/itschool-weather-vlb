0056fdfbfc9af120f16afc974f5140627d646b38
bvlucretiu <vasilebirta@gmail.com>
2022-05-25 10:24:02 +0300

    edit current-weather js

--- sections/current-weather.js
+++ sections/current-weather.js
@@ -1,27 +1,41 @@
-console.log("curent weather.js");
+// Declararea functiei pentru afisarea vremii curente. Apelul se face in alte fisiere.
+function displayCurrentWeather(city = "București") {
+  // Generam link-ul serverului, pe baza orasului.
+  const currentWeatherEndpoint = getCurrentWeatherEndpoint(city);
 
-function displayCurrentWeather() {
   fetch(currentWeatherEndpoint)
     .then((response) => response.json())
-    .then((currentWeatherData) => {
-      console.log(currentWeatherData);
-      const cityName = currentWeatherData.name;
-      const unixUtc = currentWeatherData.dt;
-      const weekDay = getWeekDay(unixUtc);
-      const hours = getHours(unixUtc);
-      const iconCode = currentWeatherData.weather[0].icon;
-      const iconSrc = getWeatheIconSrc(iconCode);
-      const description = currentWeatherData.weather[0].description;
-      const windSpeed = meterPerSecToKmPerH(currentWeatherData.wind.speed);
+    .then((data) => {
+      console.log(data);
+      // Extragem proprietatile care ne intereseaza.
+      const { name, dt, main, weather, wind } = data;
+      // getDayOfTheWeek si getHour sunt creata de noi, in utils/date.
+      const day = getDayOfTheWeek(dt);
+      // Rotunjim temperaturile.
+      const temperature = Math.round(main.temp);
+      const realFeel = Math.round(main.feels_like);
+      // Atentie! weather este un array, cu un singur element.
+      const weatherDescription = weather[0].description;
+      // getWeatherIcon si windToKmPerHour sunt create de noi, in utils/weather.
+      const weatherIcon = getWeatherIcon(weather[0].icon);
+      const windSpeed = Math.round(windToKmPerHour(wind.speed));
 
-      const currentWeatherSection = document.querySelector(".current-weather");
-      currentWeatherSection.innerHTML = `
-      <h3>${cityName}</h3>
-      <strong>${weekDay}</strong>
-      <span> ${hours}</span>
-      <img src="${iconSrc}" alt="">
-      <p>${description}</p>
-      <p>${windSpeed} Km/h </p>
+      // Afisam pe ecran informatiile extrase din API.
+      let currentWeatherContainer = document.querySelector(".current-weather");
+      currentWeatherContainer.innerHTML = `
+        <div class="px-3">
+          <div class="fs-2 mb-2"><strong>${name}</strong></div>
+          <div class="fs-4"><strong>${day}</strong>, ${hours}</div>
+          <div class="d-flex align-items-center justify-content-center">
+            <strong class="fs-1">${temperature}°C</strong>
+            <img src="${weatherIcon}" />
+          </div>
+        </div>
+        <div class="px-3">
+          <p class="fs-5">Real feel: <strong>${realFeel}°C</strong></p>
+          <p class="fs-5 text-capitalize">${weatherDescription}</p>
+          <p class="fs-5">Vânt: <strong>${windSpeed} km/h</strong></p>
+        </div>
       `;
     });
 }
