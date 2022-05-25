8f236d1cc5c18c0ae500f21b54c75b02ed28366c
bvlucretiu <vasilebirta@gmail.com>
2022-05-25 10:06:46 +0300

    edit file enpoints

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,5 +1,15 @@
+// API Key-urile NU ar trebui sa fie stocate in format text, nu este sigur.
+// Din pacate, fara un server, nu avem cum sa fim 100% safe, dar API-ul fiind gratuit, in cel mai rau caz cineva ne va bloca aplicatia temporar.
 const API_KEY = "8f936ed8b428828089878f4f2885f49d";
-const city = "București";
 
-const querryParams = `q=${city}&appid=${API_KEY}&units=metric&lang=ro`;
-const currentWeatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?${querryParams}`;
+// Construim link-urile (andpoint-urile) serverelor de la care vom primi date.
+function getCurrentWeatherEnpoint(city) {
+  // Intotdeauna, cand folosim un API, putem obtine informatii extra prin query params.
+  // lang=ro => Rezultatele vin in romana.
+  // units=metric => Unitatea de masura va fi grade celsius.
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
